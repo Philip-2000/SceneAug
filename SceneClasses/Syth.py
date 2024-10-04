94965f051ea9cd66e1f6f9d9d350bbe80e2fcca8
zr-zhou0o0 <2033616887@qq.com>
2024-10-04 11:00:41 +0800

    Syth.py

---
+++ SceneClasses/Syth.py
@@ -0,0 +1,48 @@
+from .Scne import scne, scneDs
+from .Patn import patternManager
+
+class agmt():
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+class syth():
+    def __init__(self):
+        pass
+
+    def uncond(self):
+        pass
+
+    def textcond(self):
+        pass
+
+    def roomcond(self):
+        pass
+
+class copl():
+    def __init__(self):
+        pass
+
+    def uncond(self):
+        pass
+
+    def textcond(self):
+        pass
+
+    def roomcond(self):
+        pass
+
+class rarg():
+    def __init__(self):
+        pass
+
+    def uncond(self):
+        pass
+
+    def textcond(self):
+        pass
+
+    def roomcond(self):
+        pass
